5407312fd48e9738e90d89133abb1ef215071e28
ivandi1980 <email@address.com>
2020-09-10 18:02:20 +0700

    LEGACY CODEBASE - Refactor frontend calendar

--- app/Views/layouts/template_public/template_public_navbar.php
+++ app/Views/layouts/template_public/template_public_navbar.php
@@ -1,10 +1,10 @@
-<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-darkpurple border-bottom shadow fixed-top">
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 shadow-sm fixed-top">
     <h5 class="my-0 mr-md-auto font-weight-normal">
         <a href=""><i class="fas fa-calendar-alt"></i> e-rapat</a></h5>
-    <!-- <nav class="my-2 my-md-0 mr-md-3">
-            <a class="p-2 text-dark text-decoration-none" href="<?= base_url('beranda') ?>">Beranda</a>
-            <a class="p-2 text-dark text-decoration-none" href="<?= base_url('berita') ?>">Berita</a>
-        </nav> -->
+    <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-primary text-decoration-none" href="<?= base_url('beranda') ?>">Beranda</a>
+        <a class="p-2 text-primary text-decoration-none" href="<?= base_url('berita') ?>">Berita</a>
+    </nav>
     <a class="btn btn-outline-success" href="<?= base_url('login') ?>">
         <?php
         // $sess_id = $this->session->userdata('email');
