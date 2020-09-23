ac6eec4499ffb44c28a6b97bba653e0aaae15934
ivandi1980 <ivandi.djoh@gmail.com>
2020-09-23 14:06:10 +0700

    LEGACY CODEBASE - Refactoring

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+	<!-- Cache Control -->
+	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+	<meta http-equiv="Pragma" content="no-cache" />
+	<meta http-equiv="Expires" content="0" />
+
+	<title>E-RAPAT</title>
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="<?= base_url('assets/img/transport.svg'); ?>">
+
+	<!-- Metro 4 -->
+	<link rel="stylesheet" href="<?= base_url('assets/css/metro-all.min.css'); ?>">
+</head>
+
+<body>
+	<h1>Hello, world!</h1>
+
+	<!-- Metro 4 -->
+	<script src="<?= base_url('assets/js/metro.min.js'); ?>"></script>
+</body>
+
+</html>
